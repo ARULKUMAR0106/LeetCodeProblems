42ef9ff6d1cd2827a88a248198e5f29137de1d19
ARULKUMAR <101092540+ARULKUMAR0106@users.noreply.github.com>
2025-08-31 14:10:30 +0530

    Add files via upload

---
+++ Roman Values -13.cs
@@ -0,0 +1,98 @@
+public class Solution {
+    public int RomanToInt(string s) {
+        
+     int SumofRomans=0;
+     char lastVisited='A';
+        char [] romanArray= ['M','D','V','X','L','C'];
+
+        foreach(char c  in s){
+            int currentValue=0;    
+                if(c=='I'){
+                  currentValue=1;
+                }
+                if(c=='V'){
+                    currentValue=5;
+                }
+                if(c=='X'){
+                    currentValue=10;
+                }
+                if(c=='L'){
+                    currentValue=50;
+                }
+                if(c=='C'){
+                    currentValue=100;
+                }
+                if(c=='D'){
+                    currentValue=500;
+                }
+                if(c=='M'){
+                    currentValue=1000;
+                }
+            if( romanArray.Contains(c)){
+                
+
+                int value =GetLastVisited(lastVisited,c);
+
+                if(value==0){
+                      SumofRomans=SumofRomans+currentValue;  
+                }
+                else{
+                    SumofRomans=SumofRomans+currentValue-value;
+                }
+
+            }
+            else{
+                SumofRomans=SumofRomans+currentValue;
+            }
+            lastVisited=c;
+        }
+     
+        
+
+
+
+
+
+        return SumofRomans;
+
+
+
+
+
+
+    }
+
+    private int GetLastVisited(char str,char curr){
+        switch (str){
+            case 'C':
+                if(curr=='M'){
+                    return 200;
+                }
+                if(curr=='D'){
+                    return 200;
+                }
+                break;
+            case 'I':
+                 if(curr=='V'){
+                    return 2;
+                 }
+                 if(curr=='X'){
+                    return 2;
+                 }
+                 break;
+            case 'X':
+                 if(curr=='L'){
+                    return 20;
+                 }
+                 if(curr=='C'){
+                    return 20;
+                 }
+                 
+                 break;
+        
+
+
+        }
+        return 0;
+    }
+}
