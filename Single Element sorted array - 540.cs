9b2d318ff831271db87304a949ac69af80f89e0e
ARULKUMAR <101092540+ARULKUMAR0106@users.noreply.github.com>
2025-09-12 17:56:27 +0530

    Add files via upload

---
+++ Single Element sorted array - 540.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int SingleNonDuplicate(int[] nums) {
+        
+       
+        int result =0;
+
+        for(int i=0;i<nums.Length;i++){
+          result= result^nums[i];
+        }
+
+     return result;
+
+    }
+}
