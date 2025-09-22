b10c5ede837340b719efc28d6eaf77ccad821c3b
ARULKUMAR <101092540+ARULKUMAR0106@users.noreply.github.com>
2025-09-22 20:05:20 +0530

    Update Summary Ranges -228.cs

--- Summary Ranges -228.cs
+++ Summary Ranges -228.cs
@@ -1,43 +1,33 @@
 public class Solution {
     public IList<string> SummaryRanges(int[] nums) {
-        List<string> finalValue= new List<string>();
-        if(nums.Length<1){
-           return finalValue ;
-        }
-        int start=nums[0];
-        int i=0;
-        
-       while(i<nums.Length){
-        if( i<nums.Length-1 && nums[i]+1==nums[i+1]){
-            i++;
-        }
-        else{
-            if(start==nums[i]){
-                string value= start.ToString();
-                finalValue.Add(value);
-            }
+        List<string> Listvalues= new List<string>();
+        string values="";
+        for(int i=0;i<nums.Length;i++){
+
+            if(i<nums.Length-1 && nums[i]==nums[i+1]-1){
+               if(values!=""){
+                continue;
+               }
+               else{
+                  values+=nums[i];
+               }
+            }   
             else{
-                string value=start.ToString()+"->"+nums[i].ToString();
-                finalValue.Add(value);
-            }
-            if( i<nums.Length-1 ){
-                i++;
-                start=nums[i];
-            }
-            else{
-                i++;
-            }
+                if(values!=""){
+                    values+="->"+nums[i];
+                }
+                else{
+                    values+=nums[i];
+
+                }
+                Listvalues.Add(values);
+                values="";
+
+            } 
+
         }
+        return Listvalues;
         
-       }
-       return finalValue;
-
-
-
-
-
-
-
 
     }
 }
